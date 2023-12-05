92bb0480777461630cfd2dbf49056c22909ec91f
Notfound21 <ignaciomedel03@gmail.com>
2023-12-05 16:39:05 -0300

    moviescreen con data dummy

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -5,6 +5,7 @@ import {
   SafeAreaView,
   TouchableOpacity,
   Dimensions,
+  Image,
 } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
@@ -12,15 +13,20 @@ import { ChevronLeftIcon } from "react-native-heroicons/outline";
 import { HeartIcon } from "react-native-heroicons/solid";
 import { styles, theme } from "../theme";
 import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
 
 var { width, height } = Dimensions.get("window");
 const ios = Platform.OS == "ios";
-const topMargin = ios ? "" : " mt-9";
+const topMargin = ios ? "" : " mt-10";
 
 export default function MovieScreen() {
-  const navigation = useNavigation();
-  const [isFavourite, toggleFavourite] = useState(false);
   const { params: item } = useRoute();
+  const [isFavourite, toggleFavourite] = useState(false);
+  const navigation = useNavigation();
+  const [cast, setCast] = useState([1, 2, 3, 4, 5]);
+  const [similarMovies, setSimilarMovies] = useState([1, 2, 3, 4, 5]);
+  const [loading, setLoading] = useState(false);
+  let movieName = "Spiderman 4";
   useEffect(() => {
     //Lo usamos para llamar a la API o para cualquier ejecucion que se necesite ejecutar al cargar la pagina
   }, [item]);
@@ -34,24 +40,50 @@ export default function MovieScreen() {
       <View className="w-full">
         <SafeAreaView
           className={
-            "w-full flex-row justify-between items-center px-4" + topMargin
+            " absolute w-full flex-row justify-between items-center px-4" +
+            topMargin
           }
         >
           <TouchableOpacity
             onPress={() => navigation.goBack()}
             style={styles.background}
-            className="rounded-xl p-1"
+            className="z-10 rounded-xl p-1"
           >
             <ChevronLeftIcon size="26" strokeWidth={3} color="white" />
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)}>
+          <TouchableOpacity
+            onPress={() => toggleFavourite(!isFavourite)}
+            className="z-10"
+          >
             <HeartIcon
               size="35"
               color={isFavourite ? theme.background : "white"}
             />
           </TouchableOpacity>
         </SafeAreaView>
+        <View>
+          <Image
+            source={require("../assets/images/imagesPoster1.jpg")}
+            style={{ width, height: height * 0.55 }}
+          />
+          <LinearGradient
+            colors={["transparent", "rgba(23,23,23,0.8)", "rgba(23,23,23,10)"]}
+            style={{ width, height: height * 0.4 }}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
+            className="absolute bottom-0"
+          />
+        </View>
+      </View>
+      {/* Detalles de las peliculas */}
+      <View style={{ marginTop: -(height * 0.09) }} className="space-y-3">
+        <Text className="text-white text-center text-3xl font-bold tracking-wider">
+          {movieName}
+        </Text>
+        <Text className="text-neutral-400 font-semibold text-base text-center">
+          Muy Pronto • 2024 • 170 min
+        </Text>
       </View>
     </ScrollView>
   );
